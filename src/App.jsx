72edacb315f08999f2053b156968a05f0a72b0ab
Marveeen <jmvroqueee@gmail.com>
2023-05-13 08:44:58 +0800

    feature: Add router
        - add create router dom
        - create initial pages
        - implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "products", element: <Products /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Products.jsx
@@ -0,0 +1,5 @@
+function Products() {
+  return <div>The Products Page</div>;
+}
+
+export default Products;
